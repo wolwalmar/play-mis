9ea522638524ecf4cf4e451c4cbf16ca8272cd38
WMR <wolfgang.marquardt@mieterbund.de>
2012-06-12 17:13:36 +0200

    mitgliedschaft aendern, RSV Datei hochladen

--- app/controllers/Memberships.scala
+++ app/controllers/Memberships.scala
@@ -6,14 +6,15 @@ import play.api.data._
 import play.api.data.Forms._
 import play.api.libs.json._
 
+import java.util.Date
 import views._
 import models.memberships._
 import models.persons._
 import models.finance._
 
 case class ChangePerson(salutation: String, title: String, firstname: String, lastname: String, birthday: java.util.Date)
-
 case class ChangeAddress(street: String, number: String, zip: String, city: String)
+case class ChangeMembership(begin_ms: Date, end_ms: Date, contrib: String)
 
 object Memberships extends Controller {
 	val newMembershipForm: Form[NewMembership] = Form(
@@ -61,6 +62,15 @@ object Memberships extends Controller {
     (ChangeAddress.apply)(ChangeAddress.unapply)
   )
 
+  val changeMembershipForm: Form[ChangeMembership] = Form(
+    mapping(
+          "begin_ms" -> date,
+          "end_ms" -> date,
+          "contrib" -> text
+        ) 
+    (ChangeMembership.apply)(ChangeMembership.unapply)
+  )
+
 	/**
    	 * Display an empty form.
    	*/
@@ -81,6 +91,20 @@ object Memberships extends Controller {
 	def edit(id: Long) = TODO
 
 	def update(id: Long) = TODO
+
+  def douploadrsv =  Action(parse.multipartFormData) { request =>
+    request.body.file("fileToUpload").map { picture =>
+      import java.io.File
+      val filename = picture.filename 
+      val contentType = picture.contentType
+      picture.ref.moveTo(new File("/temp/picture"))
+      Membership.loadFromFile("/temp/picture")
+      Ok("File uploaded")
+    }.getOrElse {
+      Redirect(routes.Application.index).flashing(
+        "error" -> "Missing file"
+    ) }
+  }
 
   def changePerson(membershipId: Long) = Action { implicit request =>
     changePersonForm.bindFromRequest.fold(
@@ -136,6 +160,30 @@ object Memberships extends Controller {
               )))
           })
   }
+
+  def changeMembership(membershipId: Long) = Action { implicit request =>
+    changeMembershipForm.bindFromRequest.fold(
+      errors => { println("error"); BadRequest("error") },
+      changes => {
+          println(membershipId)
+          val ms = Membership.findByMembershipId(membershipId)
+          Membership.update(
+            new Membership(
+              ms.id, 
+              membershipId,
+              changes.begin_ms,
+              changes.end_ms,
+              changes.contrib.toInt))
+          val df = new java.text.SimpleDateFormat("yyyy-MM-dd")
+          Ok(Json.toJson(
+              Map(
+                "begin_ms"->Json.toJson(df.format(changes.begin_ms)),
+                "end_ms"->Json.toJson(df.format(changes.end_ms)),
+                "contrib"->Json.toJson(changes.contrib)
+              )))
+          })
+  }
+
 	 /**
    * Handle form submission.
    */
--- app/models/memberships/Membership.scala
+++ app/models/memberships/Membership.scala
@@ -131,8 +131,63 @@ object Membership {
 		}
 	}
 
+	def update(m: Membership): Boolean = {
+		DB.withConnection {
+			implicit connection =>
+			SQL("""update
+						membership
+					set 
+						begin_ms={begin_ms},
+						end_ms={end_ms},
+						contrib={contrib},
+						ms_id={ms_id}
+					where
+						id={id}""")
+			.on(		
+				"begin_ms" -> m.begin_ms,
+				"end_ms" -> m.end_ms,
+				"contrib" -> m.contrib,
+				"ms_id" -> m.membershipId,
+				"id" -> m.id).executeUpdate == 1
+		}
+	}
+
 	def nextSeqNum: Long = DB.withConnection {
 		implicit connection =>
 			return SQL("select ms_id_seq.nextval as next from dual").as(membershipIdRSParser).head
+	}
+
+	def loadFromFile(filename: String) = {
+		def firstnameFrom(name: String) = { val s = name.split(","); if(s.length==2) s(1).trim else "" }
+		def lastnameFrom(name: String) = { val s = name.split(","); if(s.length>=1) s(0).trim else "" }
+		//ig 1,verein 3,nr 8, name30, name2 30,strasse 30, land 3, plz 5, ort 24, ig 4, von 8, bis 8, ig 3, von 8, bis 8
+		val MemberRE = """.{1}(.{3})(.{8})(.{30})(.{30})(.{30})(.{3})(.{5})(.{24}).{4}(.{8})(.{8}).{3}(.{8})(.{8}).*""".r
+		val src = scala.io.Source.fromFile(filename)
+		val sdf = new java.text.SimpleDateFormat("ddMMyyyy")
+		val list = src.getLines.foreach { (line: String) =>
+			val MemberRE(vid,mid,name1,name2,street,country,zip,city,begin_ms,end_ms,begin_rs,end_rs) = line
+        	val ms_ref = Membership.insert(new Membership(0,
+                            mid.toLong,
+                            if(begin_ms.trim.length==0) new java.util.Date() else sdf.parse(begin_ms),
+                            if(end_ms.trim.length==0) new java.util.Date() else sdf.parse(end_ms),
+                            1))
+        	Person.insert(new Person(0,
+                            "",
+                            "",
+                            firstnameFrom(name1),
+                            lastnameFrom(name1),
+                            new java.util.Date(),
+                            ms_ref))
+        
+        	Address.insert(new Address(0,
+                            street,
+                            "0",
+                            zip,
+                            city,
+                            ms_ref,
+                            None))
+
+		}
+		src.close()
 	}
 }
