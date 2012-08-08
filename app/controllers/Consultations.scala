bbdc2583ee7a3dbccf43c850678fccd050b78298
WMR <wolfgang.marquardt@mieterbund.de>
2012-08-08 16:22:28 +0200

    datails.scala.html refactored

---
+++ app/controllers/Consultations.scala
@@ -0,0 +1,23 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.libs.json._
+
+import java.util.Date
+import views._
+import models.memberships._
+import models.persons._
+import models.finance._
+
+
+
+object Consultations extends Controller {
+	def list(membershipId: Long) = Action {
+		NotImplemented
+	}
+
+
+}
